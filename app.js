7de16e3dde6f622bc827c9b481c32784cc6e59fa
Sai <109896677+GowthamVukoti@users.noreply.github.com>
2022-07-27 22:56:23 +0530

    feat:Api added
    added API

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler() 
-{
-   outputDiv.innerText="jkjakhjkjh "+ txtInput.value
+function getTranslateURL(text){
+   return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+   console.log("error occured ", error);
+   alert("Sorry its not my mistake its just you can only translate 5 times for an certain period of time so please try again after some time");
+}
+
+function clickHandler() {
+  var inputText = txtInput.value;
+
+  fetch(getTranslateURL(inputText))
+  .then(response => response.json())
+  .then(json=> {
+
+   var translatedText = json.contents.translated;
+   outputDiv.innerText = translatedText;
+  })
+   .catch(errorHandler);
+  
 };
 
 btnTranslate.addEventListener("click", clickHandler);
-
